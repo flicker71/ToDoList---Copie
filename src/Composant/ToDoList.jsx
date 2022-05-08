6b30c14738aa06542c5de6dc6d60425f04245c0e
flicker71 <72727849+flicker71@users.noreply.github.com>
2022-05-08 19:24:09 +0200

    Ignore eslint
    Trop de soucis à gérer :'(

--- src/Composant/ToDoList.jsx
+++ src/Composant/ToDoList.jsx
@@ -1,46 +1,42 @@
-import React from "react";
-import Todo from "./Todo";
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Todo from './Todo';
 
-const TodoList = ({ todos, toogleComplete, removeTodo }) => {
+function TodoList({ todos, toogleComplete, removeTodo }) {
+  const [statusTodo, setStatusTodo] = useState('all');
 
-    
-  const [statusTodo, setStatusTodo] = useState("all");
-  
-  //Affiche les élements du select
+  // Affiche les élements du select
   const options = [
     { value: '', label: 'Choisit un filtre' },
     { value: 'all', label: 'All' },
     { value: 'completed', label: 'Completed' },
     { value: 'uncompleted', label: 'Uncompleted' },
   ];
-    
+
   const handleStatusTodo = (e) => {
-    console.log("Status Selected!!");
+    console.log('Status Selected!!');
     const selectedValue = e.target.value;
     setStatusTodo({ status: selectedValue });
-    console.log({status: selectedValue});
+    console.log({ status: selectedValue });
     return selectedValue;
-  }
+  };
 
-    // const { todos } = props
-    return (
-        
-    <form >
-        <select value={statusTodo.value} onChange={handleStatusTodo}>
-            {options.map((option) => (
-            <option value={option.value}>{option.label}</option>
-            ))}
-        </select>
-        {/* Affiche ma liste des todos */}
-        <ul >
-            {todos.map(todo => (
-                <Todo key={todo.id} todo={todo} toogleComplete={toogleComplete} removeTodo={removeTodo} />
-            ))}
-        </ul>
+  // const { todos } = props
+  return (
+
+    <form>
+      <select value={statusTodo.value} onChange={handleStatusTodo}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+      {/* Affiche ma liste des todos */}
+      <ul>
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} toogleComplete={toogleComplete} removeTodo={removeTodo} />
+        ))}
+      </ul>
     </form>
-    )
+  );
 }
 
-export default TodoList
+export default TodoList;
--- src/Composant/Todo.jsx
+++ src/Composant/Todo.jsx
@@ -1,93 +1,125 @@
-import { Modal } from "@material-ui/core";
-import React, { useState } from "react";
+import { Modal } from '@material-ui/core';
+import React, { useState } from 'react';
 
-const Todo = ({ todo, toogleComplete, removeTodo }) => {
-    
+function Todo({ todo, toogleComplete, removeTodo }) {
   const [editedTodo, setEdit] = useState(false);
-//   let editedTodo = false;
+  //   let editedTodo = false;
 
-    // Change le status de mes todos
-    const handleCheckBoxClick = () => {
-        toogleComplete(todo.id);
-    }
+  // Change le status de mes todos
+  const handleCheckBoxClick = () => {
+    toogleComplete(todo.id);
+  };
 
-    //Supprime une todo
-    const handleRemoveTodo = () => {
-        removeTodo(todo.id);
-    }
+  // Supprime une todo
+  const handleRemoveTodo = () => {
+    removeTodo(todo.id);
+  };
 
-    //Ouvre la modal
-    const handleOpenModal = () => {
-        // editedTodo = true;
-        setEdit(true);
-    }
-    
-    //Ferme la modal
-    const handleCloseModal = () => {
-        // editedTodo = false;
-        setEdit(false);
-      }
-      
-    //Récupère les nouvelles valeurs dans le formualire de la modal 
-    const handleChange = (e) => {
-        setTodos({ ...todo, [e.target.name]: e.target.value });
-    };
-      
-    //Envoie les valeurs au App.js
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(todo);
-        // editedTodo = false;
-        setEdit(false);
-    };
+  // Ouvre la modal
+  const handleOpenModal = () => {
+    // editedTodo = true;
+    setEdit(true);
+  };
 
-    return (
-        <div >
-            <li style={todo.completed ? { textDecoration: 'line-through' } : {}}>
-                <input type="checkbox" checked={todo.completed} onClick={handleCheckBoxClick} />
-                <b>{' '} id : {' '}</b>
-                {todo.id}, |
-                <b>{' '} tâche à faire : {' '}</b>
-                {todo.title}, |
-                <b>{' '} description :{' '}</b>
-                {todo.description}, |
-                <b>{' '} Crée le : {' '}</b>
-                {todo.created_at}, |
-                <b>{' '} à faire pour le : {' '}</b>
-                {todo.dueDate}
-            </li>
-            <button onClick={handleRemoveTodo}> X </button>
-            <button onClick={handleOpenModal}> Edit </button>
-            
-            {/* Ma modal ne reste pas ouverte,
+  // Ferme la modal
+  const handleCloseModal = () => {
+    // editedTodo = false;
+    setEdit(false);
+  };
+
+  // Récupère les nouvelles valeurs dans le formualire de la modal
+  const handleChange = (e) => {
+    setTodos({ ...todo, [e.target.name]: e.target.value });
+  };
+
+  // Envoie les valeurs au App.js
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(todo);
+    // editedTodo = false;
+    setEdit(false);
+  };
+
+  return (
+    <div>
+      <li style={todo.completed ? { textDecoration: 'line-through' } : {}}>
+        <input type="checkbox" checked={todo.completed} onClick={handleCheckBoxClick} />
+        <b>
+          {' '}
+          {' '}
+          id :
+          {' '}
+          {' '}
+        </b>
+        {todo.id}
+        , |
+        <b>
+          {' '}
+          {' '}
+          tâche à faire :
+          {' '}
+          {' '}
+        </b>
+        {todo.title}
+        , |
+        <b>
+          {' '}
+          {' '}
+          description :
+          {' '}
+        </b>
+        {todo.description}
+        , |
+        <b>
+          {' '}
+          {' '}
+          Crée le :
+          {' '}
+          {' '}
+        </b>
+        {todo.created_at}
+        , |
+        <b>
+          {' '}
+          {' '}
+          à faire pour le :
+          {' '}
+          {' '}
+        </b>
+        {todo.dueDate}
+      </li>
+      <button onClick={handleRemoveTodo}> X </button>
+      <button onClick={handleOpenModal}> Edit </button>
+
+      {/* Ma modal ne reste pas ouverte,
             je pense que c'est à cause d'un rerender lié à un autre ocmposant mais je n'arrive pas à trouver où exactement...
              */}
-            <Modal open={editedTodo} onHide={handleCloseModal}>
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                            Titre :
-                            {' '}
-                            <input type="text" name="title" value={todo.title} placeholder="un titre" onChange={handleChange}/>
-                        </label>
-                        {' '}
-                        <label>
-                            Description :
-                            {' '}
-                            <input type="text" name="description" value={todo.description} placeholder="une description" onChange={handleChange}/>
-                        </label>
-                        {' '}
-                        <label>
-                            Date de fin :
-                            {' '}
-                            <input type="date" name="dueDate" value={todo.dueDate} onChange={handleChange}/>
-                        </label>
-                    
-                        <button type="submit">Edit</button>
-                        <br></br>
-                    </form>
-            </Modal>
+      <Modal open={editedTodo} onHide={handleCloseModal}>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Titre :
+            {' '}
+            <input type="text" name="title" value={todo.title} placeholder="un titre" onChange={handleChange} />
+          </label>
+          {' '}
+          <label>
+            Description :
+            {' '}
+            <input type="text" name="description" value={todo.description} placeholder="une description" onChange={handleChange} />
+          </label>
+          {' '}
+          <label>
+            Date de fin :
+            {' '}
+            <input type="date" name="dueDate" value={todo.dueDate} onChange={handleChange} />
+          </label>
 
-            {/* <Dialog maxWidth='lg' open={open} onClose={handleToClose}>
+          <button type="submit">Edit</button>
+          <br />
+        </form>
+      </Modal>
+
+      {/* <Dialog maxWidth='lg' open={open} onClose={handleToClose}>
                 <DialogTitle>{"Edit your todo"}</DialogTitle>
                 <DialogContentText>
                     <label>
@@ -119,8 +151,8 @@ const Todo = ({ todo, toogleComplete, removeTodo }) => {
                     </Button>
                 </DialogActions>
             </Dialog> */}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Todo;
